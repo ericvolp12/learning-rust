8ca7c09c527b2218114e4829282551dbfcbdcd59
Eric Volpert <ericvolp12@gmail.com>
2020-07-19 21:49:31 -0500

    Rustlings exercises answers

---
+++ rustlings/exercises/clippy/target/debug/deps/clippy2-283df015cf185f2b.d
@@ -0,0 +1,5 @@
+/home/user/documents/ericvolp12/learning_rust/rustlings/exercises/clippy/target/debug/deps/clippy2-283df015cf185f2b.rmeta: clippy2.rs
+
+/home/user/documents/ericvolp12/learning_rust/rustlings/exercises/clippy/target/debug/deps/clippy2-283df015cf185f2b.d: clippy2.rs
+
+clippy2.rs:
